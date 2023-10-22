7d04feebb62807d3c680d91e69b24bce43500714
Addarshkumar <addarsh.kumar@kalvium.community>
2023-10-22 11:32:04 +0530

    numeron game

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // Iteration 1: Make the Play button functional.
+var btn=document.getElementById("play-button")
 // Description: When the Play button is clicked the game page should be displayed.
+btn.addEventListener("click",redirect)
+    function redirect(){location.href = "./game.html";
+}
--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 5: Store the player score and display it on the game over screen
+
+let scoreBox=document.getElementById("score-board")
+let value=localStorage.getItem("score")
+scoreBox.textContent=value;
+
+var playAgain=document.getElementById("play-again-button");
+playAgain.onclick=()=>{
+    location.href="./game.html","_self"
+}
+
